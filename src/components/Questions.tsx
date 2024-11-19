4ed5f826240e5e77e121da9fac829f7c976d143e
Stark Industries <97692264+TDanyStark@users.noreply.github.com>
2024-11-19 14:07:16 -0500

    Agregar lógica para validar respuestas de preguntas y actualizar el estado de selección; mejorar la gestión de pasos en el componente de preguntas

--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -4,20 +4,27 @@ import { useState } from "react";
 
 const Questions = () => {
   const [preguntas, setPreguntas] = useState(data);
+  const [countClick, setCountClick] = useState(0);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCountClick(countClick + 1);
     const { name, value } = e.target;
-    const index = parseInt(name.split("-")[1]); // Obtén el índice de la pregunta
-    const letraSeleccionada = value; // Opción seleccionada por el usuario
+    const index = parseInt(name.split("-")[1]);
+
+    // Verificar si la pregunta anterior ya está respondida
+    if (index > 0) {
+      const preguntaAnterior = preguntas[index - 1];
+      if (!preguntaAnterior.selectedOption) {
+        alert("Debes responder la pregunta anterior");
+        return;
+      }
+    }
 
     const newPreguntas = preguntas.map((pregunta, i) => {
       if (i === index) {
         return {
           ...pregunta,
-          opciones: pregunta.opciones.map((opcion) => ({
-            ...opcion,
-            selected: opcion.letra === letraSeleccionada // Marca como seleccionada la opción elegida
-          }))
+          selectedOption: value as "a" | "b" | "c" | undefined,
         };
       }
       return pregunta;
@@ -26,51 +33,82 @@ const Questions = () => {
     setPreguntas(newPreguntas);
 
     // Verificar si la respuesta es correcta
-    const pregunta = preguntas[index];
-    const respuestaCorrecta = pregunta.opciones.find((opcion) => opcion.isCorrect);
+    const preguntaActual = newPreguntas[index];
+    const opcionSeleccionada = preguntaActual.opciones.find(
+      (opcion) => opcion.letra === value
+    );
 
-    if (respuestaCorrecta?.letra === letraSeleccionada) {
+    if (opcionSeleccionada?.isCorrect) {
       console.log("¡Respuesta correcta!");
     } else {
       console.log("Respuesta incorrecta");
     }
   };
 
+  const pasosCorrectos = preguntas.reduce((acc, pregunta) => {
+    const opcionCorrecta = pregunta.opciones.find((opcion) => opcion.isCorrect);
+    return opcionCorrecta?.letra === pregunta.selectedOption ? acc + 1 : acc;
+  }, 0);
+
+  const imgStep = `step${pasosCorrectos}`;
+
+  console.log("countClick", countClick);
+
   return (
     <div className="flex flex-col md:flex-row mt-8 gap-8 font-brandon">
       <div className="labyrinth basis-2/5">
         <div className="sticky top-10">
           <img
-            src={`${URL_BASE}/images/steps_labyrinth/step0.webp`}
-            alt="Paso 0"
-            className="w-full max-w-lg mx-auto"
+            src={`${URL_BASE}/images/steps_labyrinth/${imgStep}.webp`}
+            alt={`Labyrinth ${imgStep}`}
+            className="w-full max-w-lg mx-auto select-none"
+            draggable="false"
           />
+          <div className="absolute inset-0 z-10 flex justify-center items-center">
+            <div className="absolute inset-0 bg-black opacity-40"></div>
+            <p className="text-7xl font-bold relative">HOLAAAA</p>
+          </div>
         </div>
       </div>
       <div className="questions basis-3/5">
         <ol className="space-y-4">
-          {preguntas.map((pregunta, index) => (
-            <li key={index}>
-              <h2 className="text-2xl font-bold">{pregunta.pregunta}</h2>
-              <ul className="mt-2 space-y-2">
-                {pregunta.opciones.map((opcion) => (
-                  <li key={opcion.letra} className="text-xl">
-                    <label className={`py-1 px-4 border border-transparent hover:border-amarillo_abbott inline-block rounded-full cursor-pointer ${opcion.isCorrect && opcion.selected ? 'bg-amarillo_abbott text-azul_claro': ''}`}>
-                      <input
-                        className="w-4 h-4 hidden"
-                        type="radio"
-                        name={`pregunta-${index}`}
-                        value={opcion.letra}
-                        onChange={(e) => handleChange(e)}
-                        checked={opcion.selected} // Muestra la opción seleccionada
-                      />
-                      {`${opcion.letra}. ${opcion.texto}`}
-                    </label>
-                  </li>
-                ))}
-              </ul>
-            </li>
-          ))}
+          {preguntas.map((pregunta, index) => {
+            const isDisabled =
+              index > 0 && !preguntas[index - 1].selectedOption;
+            return (
+              <li key={index}>
+                <h2 className="text-2xl font-bold">{pregunta.pregunta}</h2>
+                <ul className="mt-2 space-y-2">
+                  {pregunta.opciones.map((opcion) => {
+                    const isSelected = pregunta.selectedOption === opcion.letra;
+                    const isCorrect = opcion.isCorrect && isSelected;
+                    return (
+                      <li key={opcion.letra} className="text-xl">
+                        <label
+                          className={`py-1 px-4 border border-transparent hover:border-amarillo_abbott inline-block rounded-full cursor-pointer ${
+                            isSelected
+                              ? "bg-amarillo_abbott text-azul_claro"
+                              : ""
+                          }  ${isDisabled ? "opacity-50 cursor-not-allowed" : ""}`}
+                        >
+                          <input
+                            className="hidden"
+                            type="radio"
+                            name={`pregunta-${index}`}
+                            value={opcion.letra}
+                            onChange={handleChange}
+                            checked={isSelected}
+                            disabled={isDisabled}
+                          />
+                          {`${opcion.letra}. ${opcion.texto}`}
+                        </label>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </li>
+            );
+          })}
         </ol>
       </div>
     </div>
--- src/data/questions.ts
+++ src/data/questions.ts
@@ -2,12 +2,13 @@ type Opcion = {
   letra: "a" | "b" | "c";
   texto: string;
   isCorrect: boolean;
-  selected: boolean;
 };
 
 type Pregunta = {
   pregunta: string;
   opciones: Opcion[];
+  selectedOption?: "a" | "b" | "c"; // Opción seleccionada por el usuario
+  step: string;
 };
 
 export const preguntas: Pregunta[] = [
@@ -17,68 +18,66 @@ export const preguntas: Pregunta[] = [
       {
         letra: "a",
         texto: "Desarrollo de resistencia bacteriana",
-        isCorrect: true,
-        selected: false
+        isCorrect: true
       },
       {
         letra: "b",
         texto: "Aumento de la eficacia de otros antibióticos",
-        isCorrect: false,
-        selected: false
+        isCorrect: false
       },
       {
         letra: "c",
         texto: "Aumento de los costos de producción",
-        isCorrect: false,
-        selected: false
+        isCorrect: false
       }
-    ]
+    ],
+    step: "step1"
   },
   {
     pregunta: "¿Cuál de las siguientes situaciones representa un uso indebido de azitromicina?",
     opciones: [
       {
         letra: "a",
-        texto: "Completar el tratamiento recomendado por el médico, aunque hayan desaparecido los síntomas",
-        isCorrect: false,
-        selected: false
+        texto:
+          "Completar el tratamiento recomendado por el médico, aunque hayan desaparecido los síntomas",
+        isCorrect: false
       },
       {
         letra: "b",
-        texto: "Tomar la dosis prescrita por el médico en el horario recomendado",
-        isCorrect: false,
-        selected: false
+        texto:
+          "Tomar la dosis prescrita por el médico en el horario recomendado",
+        isCorrect: false
       },
       {
         letra: "c",
-        texto: "Iniciar un tratamiento cada vez que aparezcan síntomas respiratorios para evitar una enfermedad grave",
-        isCorrect: true,
-        selected: false
+        texto:
+          "Iniciar un tratamiento cada vez que aparezcan síntomas respiratorios para evitar una enfermedad grave",
+        isCorrect: true
       }
-    ]
+    ],
+    step: "step2"
   },
   {
-    pregunta: "¿Cómo puede contribuir el uso indebido de azitromicina al problema de la resistencia bacteriana?",
+    pregunta:
+      "¿Cómo puede contribuir el uso indebido de azitromicina al problema de la resistencia bacteriana?",
     opciones: [
       {
         letra: "a",
         texto: "Al permitir que las bacterias se adapten y desarrollen resistencia",
-        isCorrect: true,
-        selected: false
+        isCorrect: true
       },
       {
         letra: "b",
         texto: "Al eliminar todas las bacterias, incluidas las beneficiosas",
-        isCorrect: false,
-        selected: false
+        isCorrect: false
       },
       {
         letra: "c",
         texto: "Al disminuir la cantidad de bacterias en el cuerpo",
-        isCorrect: false,
-        selected: false
+        isCorrect: false
       }
-    ]
+    ],
+    step: "step3"
   },
   {
     pregunta: "¿Qué debe hacerse antes de iniciar un tratamiento con azitromicina?",
@@ -86,44 +85,43 @@ export const preguntas: Pregunta[] = [
       {
         letra: "a",
         texto: "Verificar la disponibilidad del medicamento en la farmacia",
-        isCorrect: false,
-        selected: false
+        isCorrect: false
       },
       {
         letra: "b",
         texto: "Consultar con el médico y seguir sus recomendaciones",
-        isCorrect: true,
-        selected: false
+        isCorrect: true
       },
       {
         letra: "c",
         texto: "Iniciar el tratamiento con base en la experiencia personal",
-        isCorrect: false,
-        selected: false
+        isCorrect: false
       }
-    ]
+    ],
+    step: "step4"
   },
   {
-    pregunta: "¿Cómo puede el farmacéutico evitar el aumento de la resistencia bacteriana a la azitromicina?",
+    pregunta:
+      "¿Cómo puede el farmacéutico evitar el aumento de la resistencia bacteriana a la azitromicina?",
     opciones: [
       {
         letra: "a",
-        texto: "Suministrando el medicamento solo cuando haya síntomas que sugieran una infección",
-        isCorrect: false,
-        selected: false
+        texto:
+          "Suministrando el medicamento solo cuando haya síntomas que sugieran una infección",
+        isCorrect: false
       },
       {
         letra: "b",
         texto: "Recomendando su uso solo en los niños",
-        isCorrect: false,
-        selected: false
+        isCorrect: false
       },
       {
         letra: "c",
-        texto: "Suministrando el medicamento solo cuando exista una prescripción médica, sin cambiar las pautas recomendadas",
-        isCorrect: true,
-        selected: false
+        texto:
+          "Suministrando el medicamento solo cuando exista una prescripción médica, sin cambiar las pautas recomendadas",
+        isCorrect: true
       }
-    ]
+    ],
+    step: "step5"
   }
 ];
